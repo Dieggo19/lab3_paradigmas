b0df122cc8c8ffa41ffb214df23d8c045ec969ac
Dieggo19 <diego.acuna.j@usach.cl>
2022-06-14 20:15:47 -0400

    se implementa idea para la clase player y verificar si los jugadores ingresados son o no iguales

--- src/main/java/laboratorio/Card.java
+++ src/main/java/laboratorio/Card.java
@@ -33,4 +33,9 @@ public class Card extends ArrayList<Object>{
         }
         return false;
     }
+    //sobreescribir el toString de la superclase object
+    @Override
+    public String toString(){
+        return super.toString();
+    }
 }
--- src/main/java/laboratorio/Player.java
+++ src/main/java/laboratorio/Player.java
@@ -1,4 +1,34 @@
 package laboratorio;
 import java.util.ArrayList;
+
 public class Player {
+    //Atributos del player
+    private String nombre;
+    private String apellido;
+    public int puntuacion;
+
+    public Player() {
+    }
+
+    public Player(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
 }
